fa057bf8700914c7ecf64b8b075971d871c24486
user2-PC\user2 <mihyeon714@gmail.com>
2017-06-30 21:19:52 +0900

    pl/sql 사용한 view, list .. db의 cursor 사용하기

--- src/main/java/com/choa/qna/QnaDAO.java
+++ src/main/java/com/choa/qna/QnaDAO.java
@@ -1,6 +1,7 @@
 package com.choa.qna;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
@@ -39,5 +40,32 @@ public class QnaDAO {
 
 		return 0;
 	}
+	
+	public QnaDTO view(int num) throws Exception{
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("num", num);
+		sqlSession.selectOne(namespace+"view", map); // 알아서 map에 저장이 되어서 온다 
+		List<QnaDTO> list = (List<QnaDTO>)map.get("qna");//qna란 Mapper.xml 에서 설정해준 키의 이름 //여러개 받아오는 걸로 했으니...
+		return list.get(0);
+		
+	}
+	
+	public List<QnaDTO> list(int start, int last){
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("start", start);
+		map.put("last", last);
+		
+		sqlSession.selectOne(namespace+"list", map); 
+		//여기서는 굳이 list 안해도된다 우리는 map 하나를 꺼내오는 것이기 때문 //대부분 One으로 통일해준다
+		
+		List<QnaDTO> list = (List<QnaDTO>)map.get("qna");
+		return list;
+		
+	}
+	
+	
+	
 
 }
--- src/test/java/com/choa/fx1/QnaTest.java
+++ src/test/java/com/choa/fx1/QnaTest.java
@@ -1,6 +1,6 @@
 package com.choa.fx1;
 
-import static org.junit.Assert.assertEquals;
+import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +18,30 @@ public class QnaTest extends MyAbstractTestUnit {
 		
 		//assertNotNull(qnaDAO);
 		
-		
+		/*
 		QnaDTO qnaDTO = new QnaDTO();
 		qnaDTO.setNum(10);
 		qnaDTO.setWriter("q100MOD");
 		qnaDTO.setTitle("q100TMOD");
 		qnaDTO.setContents("q100CMOD");
-		
+		*/
 		//qnaDAO.write(qnaDTO);
 		//qnaDAO.update(qnaDTO);
-		assertEquals(1, qnaDAO.update(qnaDTO));
-		System.out.println("Finish TEST");
+		//assertEquals(1, qnaDAO.update(qnaDTO));
+		//System.out.println("Finish TEST");
 		//리턴 되는 것이 있는지 dao 에서 sysout 해둠
+		/*
+		QnaDTO qnaDTO  = qnaDAO.view(10);
+		System.out.println("view 테스트 = "+qnaDTO.getWriter());
+		*/
+		List<QnaDTO> list = qnaDAO.list(1, 10);
+		
+		for(QnaDTO qnaDTO : list){
+			System.out.println("list 테스트 = "+qnaDTO.getWriter());
+		}
+		
+		
+		
 	}
 
 }
